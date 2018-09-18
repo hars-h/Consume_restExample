20ddeb1d2bae79b048190e628205f5663b9536b0
Anand, Harsh <harsh.anand07@yahoo.com>
2018-09-18 11:48:14 +0530

    rest consumer with CORS enabled

---
+++ src/main/java/com/springboot/restconsumer/RestconsumerApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.restconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestconsumerApplication.class, args);
+	}
+}
