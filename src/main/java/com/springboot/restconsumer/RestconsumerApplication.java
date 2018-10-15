b27683b3ba62f9099a72cfb9a98ae6ba28372488
Anand, Harsh <harsh.anand07@yahoo.com>
2018-10-16 04:19:04 +0530

    Added Eureka client discovery

--- src/main/java/com/springboot/restconsumer/RestconsumerApplication.java
+++ src/main/java/com/springboot/restconsumer/RestconsumerApplication.java
@@ -2,11 +2,29 @@ package com.springboot.restconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
+@SuppressWarnings("deprecation")
 @SpringBootApplication
+@EnableEurekaClient
 public class RestconsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestconsumerApplication.class, args);
 	}
+	
+	 @Bean
+	   public WebMvcConfigurer corsConfigurer() {
+	      return new WebMvcConfigurerAdapter() {
+	         @Override
+	         public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/view-products").allowedOrigins("http://localhost:8080");
+	         }
+	      };
+	   }
 }
